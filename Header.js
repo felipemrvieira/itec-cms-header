c1f00421ef3fe8a1a43f35f16b973090474342c7
Felipe Maciel <felipemrvieira@gmail.com>
2019-04-12 13:27:08 -0300

    Adiciona fontes e estilo ao componente

---
+++ Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Header.scss';
+import logo from './assets/s5_logo.png';
+
+
+class Header extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <header>
+                <div className="container header-container">
+                    <div className="titulo">
+                        <img src={logo} alt="Logo Alagoas" />
+                    </div>
+                    <div className="orgao">
+                        <h5 className="orgao-titulo">{this.props.title}</h5>
+                        <p className="orgao-descricao">{this.props.name}</p>
+                    </div>
+                    <div className="busca">
+                        <input type="text" name="" id="busca-principal" />
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
